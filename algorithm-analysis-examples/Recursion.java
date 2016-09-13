e99eab9ca4be1ef89420130114608195dd96dd90
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-09-13 18:03:35 -0400

    added English Ruler example

--- algorithm-analysis-examples/Recursion.java
+++ algorithm-analysis-examples/Recursion.java
@@ -15,4 +15,31 @@ public class Recursion {
     }
   }
 
+  // English ruler example from class
+  public static void drawRuler(int numberOfInches, int majorLength) {
+    drawLine(majorLength, 0);
+    for (int j = 1; j < numberOfInches; j += 1) {
+      drawInterval(majorLength - 1);
+      drawLine(majorLength, j);
+    }
+  }
+
+  private static void drawLine(int tickLength, int tickLabel) {
+    for (int j = 0; j < tickLength; j += 1) {
+      System.out.print("-");
+    }
+    if (tickLabel >= 0) {
+      System.out.print(tickLabel);
+      System.out.print("\n");
+    }
+  }
+
+  private static void drawInterval(int centralLength) {
+    if (centralLength >= 1) {
+      drawInterval(centralLength - 1);
+      drawLine(centralLength, -1);
+      drawInterval(centralLength - 1);
+    }
+  }
+
 }
