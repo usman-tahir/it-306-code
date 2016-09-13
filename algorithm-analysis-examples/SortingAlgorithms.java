861689f85f20315b1508059c827061027f5479fe
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-09-13 12:09:51 -0400

    added selection sort with example

--- algorithm-analysis-examples/SortingAlgorithms.java
+++ algorithm-analysis-examples/SortingAlgorithms.java
@@ -3,9 +3,12 @@ public class SortingAlgorithms {
   public static void main(String[] args) {
     int[] unsorted = {12, 53, 66, 101, 1, 4, 54, 90, 109, 442, 13};
     printArray(unsorted);
-    
+
     int[] insertionSortedList = insertionSort(unsorted);
-    printArray(insertionSortedList);
+    // printArray(insertionSortedList);
+
+    int[] selectionSortedList = selectionSort(unsorted);
+    printArray(selectionSortedList);
   }
 
   /* Print out array elements */
@@ -30,6 +33,26 @@ public class SortingAlgorithms {
         j--;
       }
       tempArray[j] = currentNumber;
+    }
+    return tempArray;
+  }
+
+  public static int[] selectionSort(int[] array) {
+    int arrayLength = array.length;
+    int[] tempArray = array;
+    int i;
+    int j;
+
+    for (i = 0; i < arrayLength - 1; i++) {
+      int first = i;
+      for (j = i + 1; j < arrayLength; j++) {
+        if (tempArray[j] < tempArray[first]) {
+          first = j;
+        }
+      }
+      int temp = tempArray[first];
+      array[first] = array[i];
+      array[i] = temp;
     }
     return tempArray;
   }
