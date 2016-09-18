c5d58363384bad9eca11b8596b2f78f4ce061d02
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-09-17 21:32:52 -0400

    added monkeyTrouble function

--- coding-bat/Warmup_1.java
+++ coding-bat/Warmup_1.java
@@ -1,9 +1,9 @@
 
 public class Warmup_1 {
   public static void main(String[] args) {
-    System.out.println(sleepIn(false, false));
-    System.out.println(sleepIn(true, false));
-    System.out.println(sleepIn(false, true));
+    System.out.println(sleepIn(false, false)); // true
+    System.out.println(sleepIn(true, false)); // false
+    System.out.println(sleepIn(false, true)); // true
   }
 
   /*
@@ -15,4 +15,15 @@ public class Warmup_1 {
     // Not a weekday / on vacation
     return (!weekDay || vacation);
   }
+
+  /*
+    We have two monkeys, a and b, and the parameters aSmile and bSmile
+    indicate if each is smiling. We are in trouble if both of them are
+    smiling, or neither of them are smiling. Return if we are in trouble.
+  */
+  public static boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+    // If both monkeys match up, then return true
+    return (aSmile == bSmile);
+  }
+
 }
