3c278726b9dcda4ab78ef0de4fbedd6a9bac283c
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-09-20 17:44:36 -0400

    added methods to the Node class

--- algorithm-analysis-examples/Node.java
+++ algorithm-analysis-examples/Node.java
@@ -3,14 +3,27 @@ public class Node {
 
   private int data;
   private Node nextNode;
+  private static int size;
 
   public Node(int d, Node n) {
     this.data = d;
     this.nextNode = n;
+    size += 1;
+  }
+
+  public boolean hasNext() {
+    if (this.nextNode == null) {
+      return false;
+    }
+    return true;
   }
 
   public Node getNext() {
     return this.nextNode;
+  }
+
+  public void setNext(node nextNode) {
+    this.nextNode = nextNode;
   }
 
   public int getData() {
@@ -19,6 +32,10 @@ public class Node {
 
   public void addNodeAfter(Node n) {
     // TODO
+  }
+
+  public static int getNumberOfNodes() {
+    return size;
   }
 
 }
