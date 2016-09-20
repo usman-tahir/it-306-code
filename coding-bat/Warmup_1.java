4127c573a8769a2980a5bf0698e59cf00acd8969
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-09-19 20:19:30 -0400

    added parrotTrouble, makes10 and nearHundred

--- coding-bat/Warmup_1.java
+++ coding-bat/Warmup_1.java
@@ -47,4 +47,32 @@ public class Warmup_1 {
     return absoluteValue;
   }
 
+  /*
+    We have a loud talking parrot. The "hour" parameter is the current hour time
+    in the range 0..23. We are in trouble if the parrot is talking and the hour
+    is before 7 or after 20. Return true if we are in trouble.
+  */
+  public static boolean parrotTrouble(boolean talking, int hour) {
+    boolean beforeSeven = hour < 7;
+    boolean afterTwenty = hour > 20;
+    return (talking && (beforeSeven || afterTwenty));
+  }
+
+  /*
+    Given two ints, a and b, return true if one of them is 10 or if their sum is 10.
+  */
+  public static boolean makes10(int a, int b) {
+    return ((a == 10 || b == 10) || (a + b == 10));
+  }
+
+  /*
+    Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num)
+    computes the absolute value of a number.
+  */
+  public static boolean nearHundred(int n) {
+    boolean withinTen = (Math.abs(100 - n) <= 10);
+    boolean withinTwenty = (Math.abs(200 - n) <= 10);
+    return (withinTen || withinTwenty);
+  }
+
 }
