50be02e965cb5e26d5a3e23c8ed38c7f2c45fa66
Usman Tahir <usmantahir@Usmans-iMac.local>
2016-09-21 08:33:39 -0400

    added class activity 4 directory with Node.java template code

---
+++ activity-4/Node.java
@@ -0,0 +1,42 @@
+
+public class Node {
+
+    private int val;
+    private Node next;
+    private static int size;
+
+    public Node(int v, Node n) {
+        this.val = v;
+        this.next = n;
+    }
+
+    public boolean hasNext() {
+        if (this.next == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void setNext(Node n) {
+        this.next = n;
+        size++;
+    }
+
+    public void setVal(int v) {
+        this.val = v;
+    }
+
+    public int getVal() {
+        return this.val;
+    }
+
+    public Node getNext() {
+        return this.next;
+    }
+
+    public static int getSize() {
+        return size;
+    }
+
+}
