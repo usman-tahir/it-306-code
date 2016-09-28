bd6c0b7854d06748720a64eb178920c454ee636b
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-09-28 13:43:05 -0400

    added Stack.java code to activity five directory

---
+++ activity-five/Stack.java
@@ -0,0 +1,37 @@
+
+public class Stack {
+
+  private LNode top;
+
+  public Stack() {
+    this.top = null;
+  }
+
+  public boolean isEmpty() {
+    return (this.top == null) ? true : false;
+  }
+
+  public void push(LNode node) {
+    if (this.isEmpty()) {
+      this.top = node;
+      this.top.setNext(null);
+    } else {
+      node.setNext(this.top);
+      this.top = node;
+    }
+  }
+
+  public Character pop() {
+    if (this.isEmpty()) {
+      return null;
+    } else {
+      Character out = this.top.getData();
+      LNode oldTop = this.top;
+      top = top.getNext();
+      oldTop.setNext(null);
+      oldTop = null;
+      return out;
+    }
+  }
+
+}
