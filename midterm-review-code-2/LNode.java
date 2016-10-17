ff05d30d927578c53134a165fc9d95b3084f5cca
usman-tahir <tahir.usman.ali94@outlook.com>
2016-10-17 11:54:06 -0400

    added a new directory for midterm review; implemented LNode.java

---
+++ midterm-review-code-2/LNode.java
@@ -0,0 +1,35 @@
+
+public class LNode {
+
+  private Integer data;
+  private LNode link;
+
+  public LNode(Integer data, LNode link) {
+    this.data = data;
+    this.link = link;
+  }
+
+  public Integer getData() {
+    return this.data;
+  }
+
+  public LNode getNext() {
+    return this.link;
+  }
+
+  public void setData(Integer data) {
+    this.data = data;
+  }
+
+  public void setNext(LNode link) {
+    this.link = link;
+  }
+
+  public boolean hasNext() {
+    if (this.link == null) {
+      return false;
+    }
+    return true;
+  }
+
+}
