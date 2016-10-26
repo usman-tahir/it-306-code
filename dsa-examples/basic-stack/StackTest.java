3b69be528861c7493fee0d8115c43f5ac9bc09db
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-10-26 13:39:29 -0400

    added some elements to the Stack object in StackTest.java; added some clarifying comments

--- dsa-examples/basic-stack/StackTest.java
+++ dsa-examples/basic-stack/StackTest.java
@@ -3,8 +3,11 @@ public class StackTest {
   public static void main(String[] args) {
     Stack s = new Stack(10);
     printStack(s);
-    s.push(10);
-    s.push(20);
+
+    // Push the numbers 1-10 into the Stack (last in, first out)
+    for (int i = 1; i <= 10; i += 1) {
+      s.push(i);
+    }
     printStack(s);
   }
 
@@ -15,7 +18,7 @@ public class StackTest {
       System.out.println("This Stack has the following elements: ");
       while (!s.isEmpty()) {
         System.out.println(s.peek());
-        s.pop();
+        s.pop(); // Take elements out of the array as you view them
       }
     }
   }
