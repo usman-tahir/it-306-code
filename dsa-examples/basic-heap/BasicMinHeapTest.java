9813262f5c4ebb93aa3bdd525f5d3e73f6827e7e
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-10-27 09:54:07 -0400

    renamed the directory to repurpose it for binary min/max heaps; created an insert method for the binary min heap

---
+++ dsa-examples/basic-heap/BasicMinHeapTest.java
@@ -0,0 +1,7 @@
+
+public class BasicMinHeapTest {
+  public static void main(String[] args) {
+    BinaryMinHeap binaryMinHep = new BinaryMinHeap(10); // Initial size of 10
+    
+  }
+}
--- dsa-examples/basic-heap/BinaryMinHeap.java
+++ dsa-examples/basic-heap/BinaryMinHeap.java
@@ -8,6 +8,29 @@ public class BinaryMinHeap {
     this.heapSize = 0;
   }
 
+  public void insert(int value) {
+    if (this.heapSize == this.data.length) {
+      throw new HeapException("This Min Heap is currently full.");
+    } else {
+      this.heapSize++;
+      this.data[heapSize - 1] = value;
+      siftUp(this.heapSize - 1);
+    }
+  }
+
+  private void siftUp(int nodeIndex) {
+    int parentIndex, temp;
+    if (nodeIndex != 0) {
+      parentIndex = getParentIndex(nodeIndex);
+      if (this.data[parentIndex] > this.data[nodeIndex]) {
+        temp = this.data[parentIndex];
+        data[parentIndex] = data[nodeIndex];
+        data[nodeIndex] = temp;
+        siftUp(parentIndex);
+      }
+    }
+  }
+
   public int getMinimum() {
     if (this.isEmpty()) {
       throw new HeapException("The Min Heap is currently empty.");
