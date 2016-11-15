5c4f2936324f6c2bc6f4f959992c298179a5afc2
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-11-15 13:14:31 -0500

    added functionality for shuffling an ArrayList of cards before populating the stack

--- dsa-examples/basic-stack/deck-of-cards/Deck.java
+++ dsa-examples/basic-stack/deck-of-cards/Deck.java
@@ -1,24 +1,37 @@
+import java.util.Collections;
+import java.util.ArrayList;
 
 public class Deck {
   public static void main(String[] args) {
-    // Card[] deckOfCards = populateDeckOfCards();
-    CardStack deck = populateDeckOfCards();
-    printCardStack(deck);
+    ArrayList<Card> deckOfCards = populateDeckOfCards();
+    Collections.shuffle(deckOfCards);
+    CardStack stackOfCards = populateCardStack(deckOfCards);
+    printCardStack(stackOfCards);
   }
 
-  public static CardStack populateDeckOfCards() {
+  public static ArrayList<Card> populateDeckOfCards() {
     String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
     String[] values = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-    CardStack stack = new CardStack(52);
+    ArrayList<Card> deckOfCards = new ArrayList<Card>(52);
     int i, j;
     int k = 0;
     for (i = 0; i < suits.length; i += 1) {
       for (j = 0; j < values.length; j += 1) {
         Card c = new Card(suits[i], values[j]);
-        stack.push(c);
+        deckOfCards.add(k++, c);
       }
     }
-    return stack;
+    return deckOfCards;
+  }
+
+  public static CardStack populateCardStack(ArrayList<Card> deckOfCards) {
+    CardStack stackOfCards = new CardStack(52);
+    int i;
+    int length = deckOfCards.size();
+    for (i = 0; i < length; i += 1) {
+      stackOfCards.push(deckOfCards.get(i));
+    }
+    return stackOfCards;
   }
 
   public static void printCardStack(CardStack c) {
@@ -32,5 +45,4 @@ public class Deck {
       }
     }
   }
-
 }
