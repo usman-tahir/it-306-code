7ab065b1280624ba8cfe248caafe0a51a2bedc21
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-11-22 16:01:46 -0500

    added an implementation class for LinkedQueue

---
+++ dsa-examples/linked-list-queue/LinkedQueueImplementation.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class LinkedQueueImplementation {
+  public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+    LinkedQueue l = new LinkedQueue();
+
+    String menu = "1. insert\n2. remove\n3. peek\n4. check empty\n5. size\n6. quit\n";
+    int choice = -1;
+    do {
+      System.out.println("Select a choice from the menu:\n" + menu);
+      choice = keyboard.nextInt();
+
+      switch (choice) {
+        case 1:
+          try {
+            System.out.println("Enter an integer element to insert:");
+            l.insert(keyboard.nextInt());
+          } catch (Exception e) {
+            System.out.println("There was an error during insertion.");
+          }
+          break;
+        case 2:
+          try {
+            System.out.println("Removed element: " + l.remove().getData());
+          } catch (Exception e) {
+            System.out.println(e.getMessage());
+          }
+          break;
+        case 3:
+          try {
+            System.out.println("Peeked element: " + l.peek().getData());
+          } catch (Exception e) {
+            System.out.println(e.getMessage());
+          }
+          break;
+        case 4:
+          System.out.println("Empty status: " + l.isEmpty());
+          break;
+        case 5:
+          System.out.println("Size: " + l.getSize());
+          break;
+        case 6:
+          System.out.println("Goodbye!");
+          break;
+        default:
+          System.out.println("Invalid menu choice. Please try again.");
+          break;
+      }
+    } while (choice != 6);
+  }
+}
