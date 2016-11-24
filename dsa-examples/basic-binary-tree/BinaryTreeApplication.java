71f989d7e92af377777b49b1a0a66d0d22ef4a5e
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-11-24 09:49:43 -0500

    added a static printing method for the BinaryTreeApplication class; minor formatting fixes

--- dsa-examples/basic-binary-tree/BinaryTreeApplication.java
+++ dsa-examples/basic-binary-tree/BinaryTreeApplication.java
@@ -9,7 +9,7 @@ public class BinaryTreeApplication {
 
     do {
       try {
-        System.out.println("\n1. insert\n2. search\n3. count nodes\n4. check empty\n5. quit");
+        System.out.println("\n1. insert\n2. search\n3. count nodes\n4. check empty\n5. print\n6. quit");
         System.out.print("Enter a choice from the menu: ");
         choice = s.nextInt();
         switch (choice) {
@@ -18,7 +18,7 @@ public class BinaryTreeApplication {
             b.insert(s.nextInt());
             break;
           case 2:
-            System.out.println("Enter an integer element to search: ");
+            System.out.print("Enter an integer element to search: ");
             System.out.println("Search result: " + b.search(s.nextInt()));
             break;
           case 3:
@@ -28,6 +28,10 @@ public class BinaryTreeApplication {
             System.out.println("Empty status: " + b.isEmpty());
             break;
           case 5:
+            printBinaryTree(b);
+            break;
+          case 6:
+            System.out.println("Goodbye!");
             System.exit(0);
           default:
             System.out.println("Please enter a valid menu choice.");
@@ -37,12 +41,15 @@ public class BinaryTreeApplication {
         System.out.println("Please enter a numeric menu value.");
         choice = -1;
       }
-      System.out.print("\nPost order: ");
-      b.postOrder();
-      System.out.print("\nPre order: ");
-      b.preOrder();
-      System.out.print("\nIn order: ");
-      b.inOrder();
     } while (true);
+  }
+  public static void printBinaryTree(BinaryTree b) {
+    System.out.print("\nPost order: ");
+    b.postOrder();
+    System.out.print("\nPre order: ");
+    b.preOrder();
+    System.out.print("\nIn order: ");
+    b.inOrder();
+    System.out.println();
   }
 }
